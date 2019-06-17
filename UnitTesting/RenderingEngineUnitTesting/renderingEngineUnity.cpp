7f56dd79d4d9f01c5eed6e7ad68d8451bdb27195
Yvens Rebouças Serpa <yvensre@gmail.com>
2019-06-17 10:54:43 +0200

    Add complete single light reading test

--- UnitTesting/RenderingEngineUnitTesting/renderingEngineUnity.cpp
+++ UnitTesting/RenderingEngineUnitTesting/renderingEngineUnity.cpp
@@ -16,5 +16,25 @@ TEST_CASE("Single Light reading.") {
     CHECK(position.y == 2.0f);
     CHECK(position.z == 0.0f);
 
+    glm::vec3 direction = alight->getDirection();
+    CHECK(direction.x ==  0.0f);
+    CHECK(direction.y ==  0.0f);
+    CHECK(direction.z == -1.0f);
+
+    glm::vec3 up = alight->getUp();
+    CHECK(up.x == 0.0f);
+    CHECK(up.y == 1.0f);
+    CHECK(up.z == 0.0f);
+
+    glm::vec4 color = alight->getColor();
+    CHECK(color.r == 0.9686f);
+    CHECK(color.g == 0.8156f);
+    CHECK(color.b == 0.2117f);
+    CHECK(color.a == 1.0000f);
+
+    CHECK(alight->getIntensity() == 80.0f);
+    CHECK(alight->getSpecularPower() == 0.0f);
+    CHECK(alight->getDirectional());
+
     delete alight;
 }
