97e343b6d36ccaf96a322ddc9392a0452bea5e10
yvens <yvensre@gmail.com>
2019-10-23 19:21:05 +0200

    Add APointCloud to RenderingEngine code

---
+++ renderingEngine/RenderingEngine/Objects/apointcloud.hpp
@@ -0,0 +1,22 @@
+#pragma once 
+
+#include "../Core/amodel.hpp"
+
+struct ARay;
+struct ATriangle;
+
+class APointCloud {
+    private:
+        const AMesh& referenceMesh;
+        float raysPerUnit;
+        float pointsPerUnit;
+        AMesh pointCloudMesh;
+    public:
+        APointCloud(const AMesh& referenceMesh, float raysPerUnit, float pointsPerUnit);
+        ~APointCloud(void) = default;
+
+        const AMesh& getMesh(void) const;
+    private:
+        AMesh generatePointCloud(void);
+        bool getContactPoint(const std::vector<GLuint>& indices, const std::vector<AVertex>& vertices, const ARay& aray, glm::vec2& baryPosition, float& distance, ATriangle& atriangle, float biggerThan);
+};
