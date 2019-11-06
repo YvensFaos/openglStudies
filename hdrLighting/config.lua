dbf37f0024a15b4625aede4d849cb831fd7d3875
yvens <yvensre@gmail.com>
2019-11-06 09:49:11 +0100

    Create HDRLighting project

---
+++ hdrLighting/config.lua
@@ -0,0 +1,161 @@
+vertexShader = [[
+    #version 400
+    layout (location = 0) in vec3 vertex;
+    layout (location = 1) in vec3 normal;
+
+    uniform mat4 model;
+    uniform mat4 view;
+    uniform mat4 projection;
+    uniform vec3 viewPosition;
+
+    out vectorOut {
+        vec3 vposition;
+        vec3 vnormal;
+
+        vec3 tangentViewPos;
+        vec3 tangentFragPos;
+    } vectorOut;
+
+    out mat4 modelMatrix;
+
+    void main()
+    {
+        vectorOut.vposition = vec3(model * vec4(vertex, 1.0));
+        modelMatrix = model;
+
+        vectorOut.vnormal = normal;
+        vectorOut.tangentViewPos = viewPosition;
+        vectorOut.tangentFragPos = vectorOut.vposition;
+    
+        gl_Position = projection * view * model * vec4(vertex, 1.0);
+    }
+]]
+
+multiLightFragmentShader = [[
+    #version 400
+
+    struct Light {
+        vec3 position;
+        vec3 direction;
+        vec4 color;
+        float intensity;
+        float specularPower;
+        bool directional;
+    };
+
+    in vectorOut {
+        vec3 vposition;
+        vec3 vnormal;
+
+        vec3 tangentViewPos;
+        vec3 tangentFragPos;
+    } vectorIn;
+
+    float lightConstant = 1.0f;
+    float lightLinear = 0.09f;
+    float lightQuadratic = 0.032f;
+    float maximumIntensity = 100.0f;
+
+    uniform int numberPointLights;
+    uniform Light pointLights[10];
+    
+    out vec4 frag_colour;
+
+    vec4 calculatePointLight(int index, const vec3 norm, const vec3 viewDir) {
+        vec3 directionV = normalize(pointLights[index].position - vectorIn.tangentFragPos);
+
+        float distance = length(pointLights[index].position - vectorIn.vposition);
+        float attenuationIntensity = pointLights[index].intensity / maximumIntensity;
+        float attenuation = 1.0 / (lightConstant / attenuationIntensity + lightLinear * distance / attenuationIntensity + lightQuadratic * (distance * distance) / attenuationIntensity);
+        
+        float diff = max(dot(norm, directionV), 0.0);
+
+        vec3 diffuse =  attenuation * diff * pointLights[index].color.rgb;
+
+        vec3 reflectDir = reflect(-directionV, norm);
+        float inc = max(dot(viewDir, reflectDir), 0.0);
+        float spec = pow(inc, pointLights[index].specularPower);
+        vec4 specular = attenuation * vec4(vec3(spec), 1.0);
+        specular.a = 1.0;
+
+        return vec4(diffuse, 1.0) + specular;
+    }
+
+    in mat4 modelMatrix;
+
+    void main()
+    {          
+        vec3 norm = normalize(transpose(inverse(mat3(modelMatrix))) * vectorIn.vnormal);
+
+        //vec3 norm = normalize(vectorIn.vnormal);
+
+        vec4 pointLightResultant = vec4(0.0);
+        int clampedNumberOfPoint = min(numberPointLights, 10);
+        vec3 viewDir = normalize(vectorIn.tangentViewPos - vectorIn.tangentFragPos);
+        for(int i = 0; i < clampedNumberOfPoint; i++) 
+        {
+            pointLightResultant += calculatePointLight(i, norm, viewDir);
+        }
+        vec4 resultantLight = pointLightResultant;
+        resultantLight.a = 1.0;
+
+        frag_colour = resultantLight;
+    }
+]]
+
+models = {}
+models[1] = {file = "../3DModels/nonormalmonkey.obj", pos = { 0.0,  0.0, 3.0}, sca = {1.0, 1.0, 1.0}, rot = { 0.0,   0.0, 0.0}}
+models[2] = {file = "../3DModels/nonormalmonkey.obj", pos = { 2.0,  0.0, 1.0}, sca = {1.0, 1.0, 1.0}, rot = { 0.0,  20.0, 0.0}}
+models[3] = {file = "../3DModels/nonormalmonkey.obj", pos = {-2.0,  0.0, 1.0}, sca = {1.0, 1.0, 1.0}, rot = { 0.0, -20.0, 0.0}}
+models[4] = {file = "../3DModels/brick.obj", pos = {0.0,  -2.0, -0.0}, sca = {4.0, 4.0, 4.0}, rot = {-70.0, 0.0, 0.0}}
+models[5] = {file = "../3DModels/brick.obj", pos = {0.0, 2.5, -4.0}, sca = {4.0, 4.0, 4.0}, rot = {  0.0, 0.0, 0.0}}
+models[6] = {file = "../3DModels/nonormaltsphere.obj", pos = {0.0, 3.0, -2.0}, sca = {1.2, 1.2, 1.2}, rot = { 0.0, 0.0, 0.0}}
+
+lightIntensity = 100
+lights = {
+    {
+        pos = { 0.0, 4.0, 0.0}, 
+        dir = {0.0, 0.0, 1.0}, 
+        up = {0.0, 1.0, 0.0}, 
+        col = {227 / 255, 118 / 255, 252 / 255, 1.0}, 
+        intensity = lightIntensity, 
+        specularPower = 32.0, 
+        directional = false
+    }
+    ,
+    {
+        pos = { 2.0, -1.5, 2.0}, 
+        dir = {0.0, 0.0, 1.0}, 
+        up = {0.0, 1.0, 0.0}, 
+        col = {118 / 255, 255 / 255, 227 / 255, 1.0}, 
+        intensity = lightIntensity, 
+        specularPower = 256.0, 
+        directional = false
+    },
+    {
+        pos = {-2.0, -1.5, 2.0}, 
+        dir = {0.0, 0.0, 1.0}, 
+        up = {0.0, 1.0, 0.0}, 
+        col = {255 / 255, 227 / 255, 118 / 255, 1.0}, 
+        intensity = lightIntensity, 
+        specularPower = 256.0, 
+        directional = false
+    },
+    {
+        pos = { 0.0, 0.5, 6.0}, 
+        dir = {0.0, 0.0, 1.0}, 
+        up = {0.0, 1.0, 0.0}, 
+        col = {20 / 255, 215 / 255, 215 / 255, 1.0}, 
+        intensity = lightIntensity, 
+        specularPower = 256.0, 
+        directional = false
+    }
+} 
+
+cameraPosition = {
+    pos   = {-2.709, 5.704, 12.595},
+    dir   = {0.250, -0.331, -0.910},
+    up    = {0.088, 0.944, -0.319},
+    right = {0.964, -0.000, 0.265},
+    angle = {-74.656, -19.348}
+}
---
+++ hdrLighting/main.cpp
@@ -0,0 +1,137 @@
+// Include standard headers
+#include <stdio.h>
+#include <stdlib.h>
+
+// Include GLEW
+#include <GL/glew.h>
+
+// Include GLFW
+#include <GLFW/glfw3.h>
+
+// Include GLM
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/rotate_vector.hpp>
+
+#include <lua.hpp>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <luahandler.hpp>
+
+#include <RenderingEngine/Core/arenderer.hpp>
+#include <RenderingEngine/Core/amodel.hpp>
+#include <RenderingEngine/Core/acamera.hpp>
+#include <RenderingEngine/Core/alight.hpp>
+#include <RenderingEngine/Core/ashader.hpp>
+#include <RenderingEngine/GraphicalTools/aframebuffer.hpp>
+#include <RenderingEngine/GraphicalTools/askybox.hpp>
+#include <RenderingEngine/Utils/amacrohelper.hpp>
+#include <RenderingEngine/Utils/arenderquad.hpp>
+#include <RenderingEngine/Utils/aluahelper.hpp>
+
+#include <RenderingEngine/Objects/alightobject.hpp>
+
+#include <stb_image.h>
+
+int main(void)
+{
+	int width = 800;
+	int height = 600;
+	ARenderer arenderer(width, height, "HDR Scene");	
+	arenderer.changeClearColor(glm::vec4(0.02f, 0.02f, 0.02f, 0.0f));
+
+	LuaHandler luaHandler;
+	luaHandler.openFile("config.lua");
+	
+  	GLuint vs =  AShader::generateShader(luaHandler.getGlobalString("vertexShader"), GL_VERTEX_SHADER);
+	GLuint fs =  AShader::generateShader(luaHandler.getGlobalString("multiLightFragmentShader"), GL_FRAGMENT_SHADER);
+
+  	GLuint shaderProgramme = AShader::generateProgram(vs, fs);
+
+	GLuint modelMatrixUniform = glGetUniformLocation(shaderProgramme, "model");
+	GLuint objectsVMatrixUniform = glGetUniformLocation(shaderProgramme, "view");
+	GLuint objectsPMatrixUniform = glGetUniformLocation(shaderProgramme, "projection");
+	GLuint objectsNumberPointLightsUniform = glGetUniformLocation(shaderProgramme, "numberPointLights");
+	GLuint objectsViewPositionUniform = glGetUniformLocation(shaderProgramme, "viewPosition");
+
+	std::vector<ALight> lights = ALuaHelper::loadLightsFromTable("lights", luaHandler);
+	std::vector<ALightObject> lightObjects = ALightObject::GenerateALightObjectsFromLights(shaderProgramme, lights);
+	GLuint pointLightCount = 0;
+
+	glm::vec3 pointLightPositions[10];
+	for (unsigned int i = 0; i < lights.size(); i++)
+	{
+		if(!lights[i].getDirectional())
+		{
+			pointLightPositions[pointLightCount] = lights[i].getPosition();
+			++pointLightCount;
+		}
+	}
+
+	GLuint pointLightsPositionUniforms[10];
+	char buffer[32];
+	
+	for (unsigned int i = 0; i < pointLightCount; i++)
+	{
+		sprintf(buffer, "pointLightsPositions[%d]", i);
+		pointLightsPositionUniforms[i] = glGetUniformLocation(shaderProgramme, buffer);
+	}
+
+	std::vector<AModel*> models = ALuaHelper::loadModelsFromTable("models", &luaHandler);
+
+	ACamera& acamera = arenderer.getCamera();
+	ALuaHelper::setupCameraPosition("cameraPosition", &acamera, &luaHandler);
+	glm::mat4 projection = glm::perspective(glm::radians(acamera.getZoom()), (float) width / (float) height, acamera.getNear(), acamera.getFar());
+	glm::mat4 modelMatrix = glm::mat4(1.0f);
+
+	glm::mat4 view = acamera.getView();
+	glm::mat4 viewProjectionMatrix = projection * view;
+
+	glm::vec3 cameraPosition = acamera.getPos();
+	
+	glActiveTexture(GL_TEXTURE0);
+	do
+	{
+		projection = glm::perspective(glm::radians(acamera.getZoom()), (float) width / (float) height, acamera.getNear(), acamera.getFar());
+		view = acamera.getView();
+		viewProjectionMatrix = projection * view;
+		cameraPosition = acamera.getPos();
+		
+		arenderer.startFrame();
+
+		glViewport(0, 0, width * 2, height * 2);
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		
+		for (unsigned int i = 0; i < lightObjects.size(); i++)
+		{
+			lightObjects[i].renderLightObject(viewProjectionMatrix);
+		}
+		
+		glUseProgram(shaderProgramme);
+		glUniformMatrix4fv(objectsVMatrixUniform, 1, GL_FALSE, glm::value_ptr(view));
+		glUniformMatrix4fv(objectsPMatrixUniform, 1, GL_FALSE, glm::value_ptr(projection));
+		glUniform3f(objectsViewPositionUniform, cameraPosition.x, cameraPosition.y, cameraPosition.z);
+		glUniform1i(objectsNumberPointLightsUniform, pointLightCount);
+
+		for (unsigned int i = 0; i < lightObjects.size(); i++)
+		{
+			lightObjects[i].setupUniforms();
+		}
+		for (unsigned int i = 0; i < pointLightCount; i++)
+		{
+			glUniform3f(pointLightsPositionUniforms[i], pointLightPositions[i].x, pointLightPositions[i].y, pointLightPositions[i].z);
+		}
+
+		AModel::renderModelsInList(&models, modelMatrixUniform, shaderProgramme);
+
+		arenderer.finishFrame();
+	}
+	while(arenderer.isRunning());
+
+	glfwTerminate();
+	return 0;
+}
